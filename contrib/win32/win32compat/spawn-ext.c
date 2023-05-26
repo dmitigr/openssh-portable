201ccbc8c863301e7605426bf6b12516dc69d114
Dmitry Igrishin <dmitigr@gmail.com>
2023-05-26 20:38:15 +0300

    Allow SSH sessions for NT Authority\System account

--- contrib/win32/win32compat/spawn-ext.c
+++ contrib/win32/win32compat/spawn-ext.c
@@ -15,12 +15,28 @@ __posix_spawn_asuser(pid_t *pidp, const char *path, const posix_spawn_file_actio
 	/* use token generated from password auth if already present */
 	HANDLE user_token = NULL;
 	
+    BOOL is_user_system = FALSE;
+    if (strcmp(user, "sshd")) {
+      /*
+       * Note, __posix_spawn_asuser() is called twice upon a session
+       * establishment: on a 1st call the value of `user` is "sshd",
+       * on a 2nd call the value of `user` is a name of a session user.
+       */
+      const PSID user_sid = get_sid(user);
+      const PSID system_sid = get_sid("NT Authority\\System");
+      is_user_system = IsValidSid(user_sid) && IsValidSid(system_sid) &&
+        EqualSid(user_sid, system_sid);
+      free(system_sid);
+      free(user_sid);
+    }
+
 	if (password_auth_token)
 		user_token = password_auth_token;
 	else if (sspi_auth_user) 
 		user_token = sspi_auth_user;
 
-	if (!user_token && (user_token = get_user_token(user, 1)) == NULL) {
+    /* No token is needed for the System account. */
+	if (!is_user_system && !user_token && (user_token = get_user_token(user, 1)) == NULL) {
 		error("unable to get security token for user %s", user);
 		errno = EOTHER;
 		return -1;
